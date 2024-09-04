9c08c7eeca12700fd86c4010e467ca9d56d53112
sewkani-manya <sewkani.manya564@gmail.com>
2024-09-04 17:02:23 +0530

    checking box and typography

--- inventory_management/app/page.js
+++ inventory_management/app/page.js
@@ -1,8 +1,12 @@
+'use client'
 import Image from "next/image";
+import { useState,useEffect } from "react";
+import { firestore } from "@/firebase";
+import { Box, Typography } from "@mui/material";
 
 
 export default function Home() {
   return (
-    <>Hello World!</>
+    <Box><Typography variant="h1">Inventory Management</Typography></Box>
   );
 }
